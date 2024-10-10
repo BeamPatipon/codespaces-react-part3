33add65bf03617ef0a09f18b40675f6e2ad1ae42
BeamPatipon <150719419+BeamPatipon@users.noreply.github.com>
2024-10-10 08:51:59 +0000

    fetch product from api

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,40 +1,58 @@
-import './Shop.css'
-import { useState } from 'react';
-
-function Item(props){
-    return (<div key={props.id} onClick={()=>props.callback(props)}>
-        <img src={props.img} width={200} height={200}/>
-        id: {props.id} <br/>
-        name: {props.name}<br/>
-        price: {props.price}<br/>
-        </div>);
-}
+import './Shop.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Shop(){
-    const products=[
-        {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-        {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-        {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-        {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-        {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-        {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
-        const [cart,setCart]=useState([]);
-        function addCart(item){
-            setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}])
-        }
-        const productsList=products.map(item=><Item {...item} callback={addCart}/>)
-        const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price}
-        <button onClick={()=>{
-            setCart(cart.filter((i,_index)=>index!=_index));
-        }}>Delete</button>
-        </li>)
-        let total = 0;
-        for(let i=0 ; i<cart.length ; i++) total += cart[i].price
-        return (<><div className="grid-container">{productsList}</div>
-        <h1>Cart</h1>
-        <button onClick={()=>setCart([])}>Clear All</button>
-        <ol>{cartList}</ol>
-        <h1>Total Price : {total}</h1>
-        </>);
-        
+    function Item(props){
+        return (<div key={props.id} onClick={()=>props.callback(props)}>
+            <img src={props.img} width={200} height={200}/><br/>
+            id: {props.id} <br/>
+            name: {props.name}<br/>
+            price: {props.price}<br/>
+        </div>);
+    }
+    const [products,setProducts] = useState([]);
+    const URL = "https://glowing-journey-5ggv749r5wq5hpx49-5000.app.github.dev";
+    useEffect(() => {
+        axios.get(URL+'/api/products')
+        .then(response => {
+            setProducts(response.data);
+        })
+        .catch(error =>{
+            console.log("error");
+        });
+    },[])
+    const [cart,setCart]=useState([]);
+
+    function addCart(item){
+        setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}]);
+    }
+    function resetCart(){
+        setCart([]);
+    }
+  
+    const productList=products.map(item=><Item {...item} callback={addCart}/>);
+    const cartList = cart.map((item, index) => (
+        <li key={item.id}>
+          {item.id} {item.name} {item.price} B.
+          <button onClick={() => {
+            alert("you clicked " + index);
+            setCart(cart.filter((_, _index) => index !== _index));
+          }}>Remove</button>
+        </li>
+    ));
+      
+      
+    let total = 0;
+    for(let i=0 ; i<cart.length;i++) total+=cart[i].price;
+
+    return ( 
+        <>
+            <div className='grid-container'>{productList}</div>
+            <h1>Cart</h1>
+            <button onClick={()=>resetCart()}>Clear</button>
+            <ol>{cartList}</ol>
+            <h3>Total : {total} B.</h3>
+        </>
+    );
 }
